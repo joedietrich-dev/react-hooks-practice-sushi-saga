855d0891e94915fb1d26bcb229754faa091f927b
Joe Dietrich <joe@joedietrich.net>
2021-09-02 21:26:55 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushiList, setSushiList] = useState([]);
+  const [budget, setBudget] = useState(100);
+  const [plates, setPlates] = useState([])
+
+  const handleIsEatenClick = (id) => {
+    const eatenSushi = sushiList.find(sushi => sushi.id === id);
+    if (budget - eatenSushi.price > 0) {
+      const editedList = sushiList.map(sushi => {
+        return (sushi.id === id) ? { ...sushi, isEaten: true } : sushi;
+      });
+      setBudget(oldBudget => oldBudget - eatenSushi.price);
+      setSushiList(editedList);
+      setPlates((oldPlates) => [...oldPlates, eatenSushi])
+    }
+  }
+
+  useEffect(() => {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => setSushiList(data.map(sushi => ({ ...sushi, isEaten: false }))));
+  }, []);
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushiList} budgetRemaining={budget} onIsEatenClick={handleIsEatenClick} />
+      <Table plates={plates} budgetRemaining={budget} />
     </div>
   );
 }
--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,20 +1,19 @@
 import React from "react";
 
-function Sushi(props) {
+function Sushi({ id, img_url, name, price, isEaten, onIsEatenClick }) {
   return (
     <div className="sushi">
-      <div className="plate" onClick={/* Give me a callback! */ null}>
-        {/* Tell me if this sushi has been eaten! */}
-        {false ? null : (
+      <div className="plate" onClick={() => onIsEatenClick(id)}>
+        {isEaten ? null : (
           <img
-            src={/* Give me an image source! */ null}
-            alt={/* Give me a name! */ "Sushi"}
+            src={img_url}
+            alt={`${name} Sushi`}
             width="100%"
           />
         )}
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {name} - ${price}
       </h4>
     </div>
   );
