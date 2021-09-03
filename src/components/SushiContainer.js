59cb61967561bdbbc16a8a64d1e8fc603d665cd8
Joe Dietrich <joe@joedietrich.net>
2021-09-02 21:26:51 -0400

    commit tree 2c8a069e129196e606b0976103c65262f018f895

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, onIsEatenClick }) {
+  const [page, setPage] = useState(0);
+  const paginatedSushiList = sushis.slice(page * 4, (page * 4) + 4);
+  const sushiList = paginatedSushiList.map(sushi => (
+    <Sushi
+      key={sushi.id}
+      id={sushi.id}
+      name={sushi.name}
+      img_url={sushi.img_url}
+      price={sushi.price}
+      isEaten={sushi.isEaten}
+      onIsEatenClick={onIsEatenClick}
+    />));
+  console.log(page)
+  const handleMoreClick = () => {
+    setPage((prevPage) => (prevPage + 1 >= sushis.length / 4) ? 0 : prevPage + 1);
+  }
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiList}
+      <MoreButton handleMoreClick={handleMoreClick} />
     </div>
   );
 }
